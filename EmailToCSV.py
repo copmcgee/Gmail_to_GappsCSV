637fb27a1a2b398dc728f788d1745e26f28d5804
copmcgee <copmcgee@gmail.com>
2019-07-30 16:58:29 +1000

    working for txt file

--- EmailToCSV.py
+++ EmailToCSV.py
@@ -3,6 +3,8 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 import base64
 
+path = "c:/cisco/path.txt"
+
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 
 def main():
@@ -13,26 +15,29 @@ def main():
 		flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
 		creds = tools.run_flow(flow, store)
 	service = build('gmail', 'v1', http=creds.authorize(Http()))
-	
-    
-    # Call the Gmail API to fetch INBOX
+	    
+	# Call the Gmail API to fetch INBOX
 	results = service.users().messages().list(userId='me',labelIds = ['INBOX']).execute()
 	#messages = results.get('messages', [])
 	
-	message_id = results['messages'][3]['id']
+	message_id = results['messages'][36]['id']
 	
 	message = service.users().messages().get(userId='me', id=message_id).execute()
-	print(message['snippet'])
+	print(message)
 	for part in message['payload']['parts']:
-            if part['filename']:
-                if 'data' in part['body']:
-                    data = part['body']['data']
-                else:
-                    att_id = part['body']['attachmentId']
-                    att = service.users().messages().attachments().get(userId='me', messageId=message_id, id=att_id).execute()
-                    data = att['data']
-                file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
-                print(file_data)
+		if part['filename']:
+			print(part)
+			attachment = service.users().messages().attachments().get(userId='Me', messageId=message['id'], id=part['body']['attachmentId']).execute()
+			file_data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
+			print(file_data)
+			f = open(path, 'wb')
+			f.write(file_data)
+			f.close()
+					
+					
+
+
+
 
 if __name__ == '__main__':
 	main()
