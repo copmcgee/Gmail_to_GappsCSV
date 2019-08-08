3c991022eb74d723ed6e31be3537bf04eb0a9e01
copmcgee <copmcgee@gmail.com>
2019-08-08 13:09:29 +1000

    Added Get_Attachment function, search for specific sender, adding date to filename

--- EmailToCSV.py
+++ EmailToCSV.py
@@ -2,13 +2,33 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import base64
+import datetime as dt
 
 store_dir = "c:/cisco/"
-
+today = dt.datetime.today().date()
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+
+
+
+def Get_Attachments(message, service): #Downloads attachment and adds date
+	for part in message['payload']['parts']:
+		if part['filename']:			
+			attachment = service.users().messages().attachments().get(userId='Me', messageId=message['id'], id=part['body']['attachmentId']).execute()
+			file_data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
+			path = ''.join([store_dir, '.'.join([str(today), part['filename']])]) #join date to filename, then to storepath
+			f = open(path, 'wb')
+			f.write(file_data)
+			f.close()
+
+
+
+
+
+
 
 def main():
    
+	#Check for store creds, otherwise you have to auth them
 	store = file.Storage('token.json')
 	creds = store.get()
 	if not creds or creds.invalid:
@@ -18,22 +38,21 @@ def main():
 	    
 	# Call the Gmail API to fetch INBOX
 	results = service.users().messages().list(userId='me',labelIds = ['INBOX']).execute()
-	#messages = results.get('messages', [])
+	messagesID = results.get('messages', []) #returns Dict with {'id': 'value', 'threadId': 'value'} of inbox messages
 	
-	message_id = results['messages'][6]['id']
 	
-	message = service.users().messages().get(userId='me', id=message_id).execute()
-	#print(message)
-	for part in message['payload']['parts']:
-		if part['filename']:
-			print(part)
-			attachment = service.users().messages().attachments().get(userId='Me', messageId=message['id'], id=part['body']['attachmentId']).execute()
-			file_data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
-			print(file_data)
-			path = ''.join([store_dir, part['filename']])
-			f = open(path, 'wb')
-			f.write(file_data)
-			f.close()
+	
+	for message in messagesID[:15]: #sorts through messages via thier ID
+		msg = service.users().messages().get(userId='me', id=message['id']).execute()
+		headrs = msg['payload']['headers'] #headers include to, from, subject, date and more
+
+		for item in headrs:
+			if item['name'] == 'From' and item['value'] == 'myemail': # ['name':'From'] in headrs that has ['value': 'myemail']
+				Get_Attachments(msg, service)
+
+
+	
+
 					
 					
 
